79e63c8b054e7218f7b3173ce17ff752234604cf
Ara Gamaliel Boanerges <aragamalielboanerges@gmail.com>
2023-02-09 10:02:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-const { ALL_USERS, generateUsers } = require("./generateUsers");
-const { ALL_TWEETS } = require("./generateTweets");
+const generateDatabase = require("./index.js");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(generateDatabase());
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 1919;
+server.use(middlewares);
+server.use(router);
 
-const generateDatabase = () => {
-  const thisUser = generateUsers(1);
-
-  return {
-    users: [...thisUser, ...ALL_USERS],
-    this_user: thisUser,
-    tweets: [...ALL_TWEETS],
-  };
-};
-
-module.exports = generateDatabase;
+server.listen(port);
