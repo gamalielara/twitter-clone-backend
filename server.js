37d272491838cd0b3d0dcf87e1414451a8b7a7e3
Ara Gamaliel Boanerges <aragamalielboanerges@gmail.com>
2023-02-09 22:18:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const generateDatabase = require("./index.js");
+const DATABASE = generateDatabase();
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(generateDatabase());
+const router = jsonServer.router(DATABASE);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 1919;
 server.use(middlewares);
